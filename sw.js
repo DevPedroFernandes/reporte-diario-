a6b47751e59dd5d83be11c68960ed2841fe1078c
Pedro Fernandes <87543276+DevPedroFernandes@users.noreply.github.com>
2026-06-16 15:51:43 -0300

    Implement service worker for caching files

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'reporte-bv-v1';
+const FILES = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
